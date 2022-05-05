7786a2e508480b332d0fed08398ab4e12819600a
seed1371 <bakatesu0307@gmail.com>
2022-05-05 22:09:15 +0900

    日報indexアクションの作成

--- src/main/java/actions/views/ReportConverter.java
+++ src/main/java/actions/views/ReportConverter.java
@@ -5,11 +5,17 @@ import java.util.List;
 
 import models.Report;
 
-// 日報データのDTOモデル⇔Viewモデルの変換を行うクラス
+/**
+ * 日報データのDTOモデル⇔Viewモデルの変換を行うクラス
+ *
+ */
 public class ReportConverter {
-    // ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
-    // @param rv ReportViewのインスタンス
-    // @return Reportのインスタンス
+
+    /**
+     * ViewモデルのインスタンスからDTOモデルのインスタンスを作成する
+     * @param rv ReportViewのインスタンス
+     * @return Reportのインスタンス
+     */
     public static Report toModel(ReportView rv) {
         return new Report(
                 rv.getId(),
@@ -19,14 +25,16 @@ public class ReportConverter {
                 rv.getContent(),
                 rv.getCreatedAt(),
                 rv.getUpdatedAt());
-
     }
 
-    // DTOモデルのインスタンスからViewモデルのインスタンスを作成する
-    // @param r Reportのインスタンス
-    // @return ReportViewのインスタンス
+    /**
+     * DTOモデルのインスタンスからViewモデルのインスタンスを作成する
+     * @param r Reportのインスタンス
+     * @return ReportViewのインスタンス
+     */
     public static ReportView toView(Report r) {
-        if(r == null) {
+
+        if (r == null) {
             return null;
         }
 
@@ -37,26 +45,29 @@ public class ReportConverter {
                 r.getTitle(),
                 r.getContent(),
                 r.getCreatedAt(),
-                r.getUpdateAt());
+                r.getUpdatedAt());
     }
 
-    // DTOモデルのインスタンスからViewモデルのリストを作成する
-    // @param list DTOモデルのリスト
-    // @return Viewモデルのリスト
-    public static List<ReportView> toViewList(List<Report> list){
+    /**
+     * DTOモデルのリストからViewモデルのリストを作成する
+     * @param list DTOモデルのリスト
+     * @return Viewモデルのリスト
+     */
+    public static List<ReportView> toViewList(List<Report> list) {
         List<ReportView> evs = new ArrayList<>();
 
-        for(Report r:list) {
+        for (Report r : list) {
             evs.add(toView(r));
         }
 
         return evs;
     }
 
-    // Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
-    // @param r DTOモデル(コピー先)
-    // @param rv Viewモデル(コピー元)
-
+    /**
+     * Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+     * @param r DTOモデル(コピー先)
+     * @param rv Viewモデル(コピー元)
+     */
     public static void copyViewToModel(Report r, ReportView rv) {
         r.setId(rv.getId());
         r.setEmployee(EmployeeConverter.toModel(rv.getEmployee()));
@@ -64,6 +75,8 @@ public class ReportConverter {
         r.setTitle(rv.getTitle());
         r.setContent(rv.getContent());
         r.setCreatedAt(rv.getCreatedAt());
-        r.setUpdateAt(rv.getUpdatedAt());
+        r.setUpdatedAt(rv.getUpdatedAt());
+
     }
+
 }
