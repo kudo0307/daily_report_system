a5867e22a631f9b024f1504ed0fab1a451bdd12a
kudou.sho <kudou.sho0307@gmail.com>
2022-05-09 20:57:24 +0900

    Timecardモデル、TimecardViewモデル作成

---
+++ src/main/java/actions/views/TimecardConverter.java
@@ -0,0 +1,88 @@
+package actions.views;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Timecard;
+
+// タイムカードデータのDTOモデル⇔Viewモデルの変換を行うクラス
+
+public class TimecardConverter {
+    // ViewモデルのインスタンスからDTOモデルのインスタンスを生成する
+    // @param tv TimecardViewのインスタンス
+    // @return Timecardのインスタンス
+
+    public static Timecard toModel(TimecardView tv) {
+
+        return new Timecard(
+                tv.getId(),
+                tv.getEmployee_id(),
+                tv.getAttendance_at(),
+                tv.getLeaving_at(),
+                tv.getRest_start_at(),
+                tv.getRest_end_at(),
+                tv.getWork_at(),
+                tv.getRest_at(),
+                tv.getCreated_at(),
+                tv.getUpdated_at(),
+                tv.getDeleted_at()
+
+                );
+
+    }
+
+    // DTOモデルのインスタンスからViewモデルのインスタンスを生成する
+    public static TimecardView toView(Timecard t) {
+
+        if(t == null) {
+            return null;
+        }
+
+        return new TimecardView(
+                t.getId(),
+                t.getEmployee_id(),
+                t.getAttendance_at(),
+                t.getLeaving_at(),
+                t.getRest_start_at(),
+                t.getRest_end_at(),
+                t.getWork_at(),
+                t.getRest_at(),
+                t.getCreated_at(),
+                t.getUpdated_at(),
+                t.getDeleted_at()
+
+                );
+    }
+
+    // DTOモデルのリストからViewモデルのリストを作成する
+    // @param list DTOモデルのリスト
+    // @return Viewモデルのリスト
+    public static List<TimecardView> toViewList(List<Timecard> list){
+
+        List<TimecardView> tvs = new ArrayList<>();
+
+        for(Timecard t : list) {
+            tvs.add(toView(t));
+        }
+
+        return tvs;
+    }
+
+    // Viewモデルの全フィールドの内容をDTOモデルのフィールドにコピーする
+    // @param t DTOモデル(コピー先)
+    // @param tv Viewモデル(コピー先)
+    public static void copyViewToModel(Timecard t,TimecardView tv) {
+        t.setId(tv.getId());
+        t.setEmployee_id(tv.getEmployee_id());
+        t.setAttendance_at(tv.getAttendance_at());
+        t.setLeaving_at(tv.getLeaving_at());
+        t.setRest_start_at(tv.getRest_start_at());
+        t.setRest_end_at(tv.getRest_end_at());
+        t.setWork_at(tv.getWork_at());
+        t.setRest_at(tv.getRest_at());
+        t.setCreated_at(tv.getCreated_at());
+        t.setUpdated_at(tv.getUpdated_at());
+        t.setDeleted_at(tv.getDeleted_at());
+    }
+
+}
---
+++ src/main/java/actions/views/TimecardView.java
@@ -0,0 +1,51 @@
+package actions.views;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+// タイムカード情報について画面の入力値・出力値を扱うViewモデル
+
+@Getter // 全てのクラスフィールドについてgetterを自動生成する(Lombok)
+@Setter // 全てのクラスフィールドについてsetterを自動生成する(Lombok)
+@NoArgsConstructor // 引数なしコンストラクタを自動生成する(Lombok)
+@AllArgsConstructor // 全てのクラスフィールドを引数に持つ引数ありコンストラクタを自動生成する(Lombok)
+public class TimecardView {
+
+    // id
+    private Integer id;
+
+    //日報を作成した従業員のid
+    private Integer employee_id;
+
+    // 出勤時間
+    private LocalDateTime attendance_at;
+
+    // 退勤時間
+    private LocalDateTime leaving_at;
+
+    // 休憩開始時間
+    private LocalDateTime rest_start_at;
+
+    // 休憩終了時間
+    private LocalDateTime rest_end_at;
+
+    // 労働時間
+    private LocalDateTime work_at;
+
+    // 休憩時間
+    private LocalTime rest_at;
+
+    // 登録日時
+    private LocalDateTime created_at;
+
+    // 更新日時
+    private LocalDateTime updated_at;
+
+    // 削除日時
+    private LocalDateTime deleted_at;
+}
